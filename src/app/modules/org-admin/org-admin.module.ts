5653c26ca2171eae6b42f673b591ed9e4b1ba081
Gagan Soni <gagan@kitaboo.com>
2024-03-18 15:09:57 +0530

    project components for org admin

--- src/app/modules/org-admin/org-admin.module.ts
+++ src/app/modules/org-admin/org-admin.module.ts
@@ -4,12 +4,16 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { orgAdminRoutes } from './org-admin.routes';
 import { SharedModule } from '../../shared/modules/shared.module';
+import { ProjectsComponent } from './projects/projects.component';
+import { ViewProjectsComponent } from './view-projects/view-projects.component';
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    ProjectsComponent,
+    ViewProjectsComponent,
   ],
   imports: [
     CommonModule,
---
+++ src/app/modules/org-admin/view-projects/view-projects.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-projects',
+  templateUrl: './view-projects.component.html',
+  styleUrl: './view-projects.component.scss'
+})
+export class ViewProjectsComponent {
+
+}
