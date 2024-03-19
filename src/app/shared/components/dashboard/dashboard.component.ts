eeba5b5c2c507eada42675c81dfe36acf4a8bf96
simran <sovesiyasimran@gmail.com>
2024-03-19 18:44:38 +0530

    added click event for cards

--- src/app/shared/components/dashboard/dashboard.component.ts
+++ src/app/shared/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EMPLOYEES } from '../employees/employees';
+import { PROJECTS } from '../../../modules/org-sales/projects/projects';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,12 +9,21 @@ import { EMPLOYEES } from '../employees/employees';
 })
 export class DashboardComponent implements OnInit{
 empData = EMPLOYEES;
-projects:number = 4;
+projects:number = PROJECTS.length || 0;;
 hasProject:number = 0;
 hasNoProject:number = 0;
 
 ngOnInit(): void {
  this.hasProject =  this.empData.filter(emp => emp.hasProject).length;
- this.hasNoProject =  this.empData.filter(emp => !emp.hasProject).length
+ this.hasNoProject =  this.empData.filter(emp => !emp.hasProject).length;
 }
+
+redirectToEmp(){
+
+}
+
+redirectToProject(){
+  
+}
+
 }
